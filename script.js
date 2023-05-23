b66beb9d1005228a0edba52e88b3c7bc9a8414ce
Álvaro Castillo <alvarocastillocabero@gmail.com>
2023-05-23 23:48:13 +0200

    Data diciembre y enero

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-// Obtén una referencia al elemento del mapa en el HTML
-var map = L.map('map').setView([41.3851, 2.1734], 12);
+document.addEventListener('DOMContentLoaded', function() {
+    var map = L.map('map').setView([41.3851, 2.1734], 12);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+    }).addTo(map);
 
-// Carga tu archivo JSON con los datos de timelineObjects
-fetch('datos_diciembre.json')
-    .then(response => response.json())
-    .then(data => {
-        var markers = L.markerClusterGroup(); // Crea un grupo de clustering de marcadores
+    var btnDiciembre = document.getElementById('btn-diciembre');
+    var btnEnero = document.getElementById('btn-enero');
 
-        data.timelineObjects.forEach(obj => {
-            if (obj.placeVisit) {
-                var lat = obj.placeVisit.location.latitudeE7 / 1e7;
-                var lng = obj.placeVisit.location.longitudeE7 / 1e7;
-                var startTimestamp = obj.placeVisit.duration.startTimestamp;
-                var endTimestamp = obj.placeVisit.duration.endTimestamp;
+    var urlDiciembre = 'data_diciembre.json';
+    var urlEnero = 'data_enero.json';
 
-                var startDate = new Date(Date.parse(startTimestamp));
-                var endDate = new Date(Date.parse(endTimestamp));
-
-                var contenidoMarcador = '<strong>' + obj.placeVisit.location.name + '</strong><br>' +
-                    'Fecha de entrada: ' + startDate.toLocaleString() + '<br>' +
-                    'Fecha de salida: ' + endDate.toLocaleString();
-
-                var marker = L.marker([lat, lng]).bindPopup(contenidoMarcador);
-                markers.addLayer(marker); // Agrega el marcador al grupo de clustering
-            }
-        });
-
-        map.addLayer(markers); // Agrega el grupo de clustering al mapa
+    btnDiciembre.addEventListener('click', function() {
+        cargarDatos(urlDiciembre);
     });
+
+    btnEnero.addEventListener('click', function() {
+        cargarDatos(urlEnero);
+    });
+
+    function cargarDatos(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.timelineObjects.forEach(obj => {
+                    if (obj.placeVisit) {
+                        var lat = obj.placeVisit.location.latitudeE7 / 1e7;
+                        var lng = obj.placeVisit.location.longitudeE7 / 1e7;
+                        var startTimestamp = obj.placeVisit.duration.startTimestamp;
+                        var endTimestamp = obj.placeVisit.duration.endTimestamp;
+
+                        var startDate = new Date(Date.parse(startTimestamp));
+                        var endDate = new Date(Date.parse(endTimestamp));
+
+                        var offsetLat = lat + (Math.random() - 0.5) * 0.0002;
+                        var offsetLng = lng + (Math.random() - 0.5) * 0.0002;
+
+                        var contenidoMarcador = '<strong>' + obj.placeVisit.location.name + '</strong><br>' +
+                            'Fecha de entrada: ' + startDate.toLocaleString() + '<br>' +
+                            'Fecha de salida: ' + endDate.toLocaleString();
+
+                        L.marker([offsetLat, offsetLng]).addTo(map)
+                            .bindPopup(contenidoMarcador);
+                    }
+                });
+            });
+    }
+});
